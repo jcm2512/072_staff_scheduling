9cf15ba76b3860fc9d36bd8955fbf7bc37ac1720
jcm2512 <joe.magalotti@gmail.com>
2025-03-16 13:37:34 +0900

    fix(CalendarView): ensure calendar header expands fully with min-width

--- src/features/views/CalendarView.tsx
+++ src/features/views/CalendarView.tsx
@@ -1,32 +1,42 @@
-import { useState } from "react";
-import {
-  Group,
-  Grid,
-  Text,
-  Paper,
-  Indicator,
-  Stack,
-  Flex,
-} from "@mantine/core";
+import { Paper, Stack } from "@mantine/core";
 import { Calendar, DatesProvider, MonthPicker } from "@mantine/dates";
-import dayjs from "dayjs";
 
 export function CalendarView() {
   return (
-    <Paper shadow="xs" p="md" w={"100%"}>
+    <Paper
+      shadow="xs"
+      p="md"
+      w={"100%"}
+      styles={{
+        root: { backgroundColor: "purple" },
+      }}
+    >
       <DatesProvider settings={{ consistentWeeks: true }}>
         <Calendar
           firstDayOfWeek={0}
           size="lg"
           maxLevel="month"
-          static
           styles={{
+            calendarHeader: {
+              backgroundColor: "red",
+              minWidth: "100%",
+            },
+            calendarHeaderLevel: {
+              backgroundColor: "orange",
+            },
+            month: {
+              backgroundColor: "green",
+              width: "100%",
+            },
             levelsGroup: {
               justifyContent: "center",
+              width: "100%",
+              backgroundColor: "yellow",
             },
             day: {
               border: "solid",
               height: "auto",
+              width: "100%",
             },
           }}
           renderDay={(date) => {
