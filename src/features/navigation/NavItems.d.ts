f3ce31d144f14f6c90e4f8b5a41e9a27a77a3169
jcm2512 <joe.magalotti@gmail.com>
2025-03-17 23:55:10 +0900

    style: simplified cell borders

--- src/features/navigation/NavItems.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "./NavItems" {
-    import React from "react";
-    export const NavItems: React.FC;
-  }
--- src/features/views/CalendarView.tsx
+++ src/features/views/CalendarView.tsx
@@ -48,74 +48,87 @@ export function CalendarView() {
   }, []);
 
   return (
-    <Paper
-      shadow="xs"
-      p="sm"
-      style={{
-        borderRadius: `var(--mantine-radius-lg)`,
-        height: "fit-content",
-      }}
-    >
-      <DatesProvider settings={{ consistentWeeks: true }}>
-        <Calendar
-          firstDayOfWeek={0}
-          withCellSpacing={false}
-          maxLevel="month"
-          style={{ borderSpacing: "0" }}
-          styles={{
-            calendarHeader: {
-              minWidth: "100%",
-              margin: "0px",
-            },
-            month: {
-              width: "100%",
-            },
+    // <Paper
+    //   shadow="xs"
+    //   p="sm"
+    //   style={{
+    //     borderRadius: `var(--mantine-radius-lg)`,
+    //     height: "fit-content",
+    //   }}
+    // >
+    <DatesProvider settings={{ consistentWeeks: true }}>
+      <Calendar
+        firstDayOfWeek={0}
+        withCellSpacing={false}
+        maxLevel="month"
+        style={{
+          borderSpacing: "0",
+          height: "fit-content",
+        }}
+        styles={{
+          calendarHeader: {
+            minWidth: "100%",
+            margin: "0px", // remove empty space between calendar header and body
+          },
+          month: {
+            width: "100%",
+          },
+          monthTbody: {
+            borderStyle: "hidden", // removes outside border on table
+          },
+          levelsGroup: {
+            justifyContent: "center",
+            width: "100%",
+            border: "2px solid #eaeaea",
+            borderRadius: `var(--mantine-radius-lg)`,
+            overflow: "hidden",
+            boxSizing: "border-box",
+            backgroundColor: "white",
+          },
 
-            levelsGroup: { justifyContent: "center", width: "100%" },
-            monthCell: {
-              // backgroundColor: "#f3f6f7",
-              borderWidth: "1px",
-              border: "solid 1px #eaeaea",
+          monthCell: {
+            // backgroundColor: "#f3f6f7",
+            borderWidth: "1px",
+            border: "solid 1px #eaeaea",
+            lineHeight: "0",
+          },
+          day: {
+            height: "100%",
+            width: "100%",
+          },
+        }}
+        renderDay={(date) => {
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, "0"); // Ensure two-digit month
 
-              lineHeight: "0",
-            },
-            day: {
-              height: "100%",
-              width: "100%",
-            },
-          }}
-          renderDay={(date) => {
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, "0"); // Ensure two-digit month
+          const day = date.getDate();
+          const formattedDate = `${year}-${month}-${day}`; // Dynamic YYYY-MM-DD
+          const daySchedule = schedule[formattedDate] || {};
 
-            const day = date.getDate();
-            const formattedDate = `${year}-${month}-${day}`; // Dynamic YYYY-MM-DD
-            const daySchedule = schedule[formattedDate] || {};
+          return (
+            <Stack align="center" h={"4.5rem"} gap={"0"}>
+              <Text size="xs">{day}</Text>
 
-            return (
-              <Stack align="center" h={"4.5rem"} gap={"0"}>
-                <Text size="xs">{day}</Text>
-
-                <Text
-                  c={daySchedule.am == "Office" ? "gray" : "cyan"}
-                  inline={true}
-                  size="xs"
-                  m={"xs"}
-                >
-                  {daySchedule.am || ""}
-                </Text>
-                <Text
-                  c={daySchedule.pm == "Office" ? "gray" : "green"}
-                  inline={true}
-                  size="xs"
-                >
-                  {daySchedule.pm || ""}
-                </Text>
-              </Stack>
-            );
-          }}
-        />
-      </DatesProvider>
-    </Paper>
+              <Text
+                c={daySchedule.am == "Office" ? "gray" : "cyan"}
+                inline={true}
+                size="xs"
+                m={"xs"}
+              >
+                {daySchedule.am || ""}
+              </Text>
+              <Text
+                c={daySchedule.pm == "Office" ? "gray" : "green"}
+                inline={true}
+                size="xs"
+              >
+                {daySchedule.pm || ""}
+              </Text>
+            </Stack>
+          );
+        }}
+      />
+    </DatesProvider>
+    // </Paper>
   );
 }
