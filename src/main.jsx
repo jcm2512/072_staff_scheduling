a62499540cd4208af390d0d39bb3f318cdc8131d
jcm2512 <joe.magalotti@gmail.com>
2025-03-17 23:14:28 +0900

    fix(main.jsx): move AuthProvider to parent module

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
-// Import styles of packages that you've installed.
-// All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
 
-import { theme } from "./themes/colors.jsx";
+import { AuthProvider } from "@/auth/AuthProvider.js";
+
+import { App } from "@/App.jsx";
+import { theme } from "@/themes/colors.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </MantineProvider>
   </StrictMode>
 );
