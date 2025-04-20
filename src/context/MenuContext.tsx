d3888d6db7e307fef95525cefb5d7328a8f126aa
jcm2512 <joe.magalotti@gmail.com>
2025-04-21 00:07:14 +0900

    feat: init menu context

---
+++ src/context/MenuContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useState } from "react";
+
+type SelectedMenu = "home" | "calendar";
+
+type MenuContextType = {
+  selectedMenu: SelectedMenu;
+  setSelectedMenu: (val: SelectedMenu) => void;
+};
+
+const MenuContext = createContext<MenuContextType | undefined>(undefined);
+
+export const MenuProvider = ({ children }: { children: React.ReactNode }) => {
+  // Hooks
+  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>("calendar");
+
+  return (
+    <MenuContext.Provider
+      value={{
+        selectedMenu,
+        setSelectedMenu,
+      }}
+    >
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+export const useMenuContext = () => {
+  const context = useContext(MenuContext);
+  if (!context) {
+    throw new Error("useMenuContext must be used within a MenuProvider");
+  }
+  return context;
+};
